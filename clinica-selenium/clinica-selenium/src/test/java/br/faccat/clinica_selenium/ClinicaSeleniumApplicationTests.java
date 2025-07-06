31ed5d30637235a78b5900f18a42482ea77b3c00
Luan Custódio <luancustodio@sou.faccat.br>
2025-07-05 21:56:54 -0300

    adicionados os testes

--- clinica-selenium/clinica-selenium/src/test/java/br/faccat/clinica_selenium/ClinicaSeleniumApplicationTests.java
+++ clinica-selenium/clinica-selenium/src/test/java/br/faccat/clinica_selenium/ClinicaSeleniumApplicationTests.java
@@ -2,12 +2,32 @@ package br.faccat.clinica_selenium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import Model.Consulta;
+import Service.ConsultaService;
 
 @SpringBootTest
 class ClinicaSeleniumApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testAssociarConsultaAPacientePorCpf() {
+		Paciente paciente = new Paciente();
+		paciente.setNome("Maria");
+		paciente.setCpf("12345678900");
+
+		pacienteRepository.save(paciente);
+
+		Consulta consulta = new Consulta();
+		consulta.setDescricao("Consulta de rotina");
+
+		ConsultaService.adicionarConsultaAoPaciente("12345678900", consulta);
+
+		List<Consulta> consultas = ConsultaService.listarConsultasPorCpf("12345678900");
+		assertEquals(1, consultas.size());
+		assertEquals("Consulta de rotina", consultas.get(0).getDescricao());
 	}
 
 }
